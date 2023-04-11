1c4a3fa4cc365ca070cb3d3d0229b97627f1f81e
SharpDevOps10 <td090833@gmail.com>
2023-04-11 22:17:19 +0300

    Improved code formatting using eslint

--- server/router.js
+++ server/router.js
@@ -1,15 +1,15 @@
 'use strict';
-const {parse} = require('node : url');
+const { parse } = require('node : url');
 module.exports = class Router {
   constructor() {
     this.routes = [];
   }
   add(method, url, handler) {
-    this.routes.push({method, url, handler});
+    this.routes.push({ method, url, handler });
   }
   resolve(context, request) {
     const path = parse(request.url).pathname;
-    for (const {method, url, handler} of this.routes) {
+    for (const { method, url, handler } of this.routes) {
       const match = url.exec(path);
       if (!match || request.method !== method) continue;
       const urlParts = match.slice(1).map(decodeURIComponent);
