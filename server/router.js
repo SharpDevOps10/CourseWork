00a3d8a9576ab122d909b9547983f4c3322569b4
SharpDevOps10 <td090833@gmail.com>
2023-04-11 22:16:35 +0300

    Changed let into const

--- server/router.js
+++ server/router.js
@@ -8,11 +8,11 @@ module.exports = class Router {
     this.routes.push({method, url, handler});
   }
   resolve(context, request) {
-    let path = parse(request.url).pathname;
+    const path = parse(request.url).pathname;
     for (const {method, url, handler} of this.routes) {
-      let match = url.exec(path);
+      const match = url.exec(path);
       if (!match || request.method !== method) continue;
-      let urlParts = match.slice(1).map(decodeURIComponent);
+      const urlParts = match.slice(1).map(decodeURIComponent);
       return handler(context, ...urlParts, request);
     }
     return null;
