cbf2f989f55854a0524c2fbac7fb88755d029f2c
SharpDevOps10 <td090833@gmail.com>
2023-04-11 22:09:57 +0300

    Implemented `resolve` method

--- server/router.js
+++ server/router.js
@@ -6,5 +6,14 @@ module.exports = class Router {
   add(method, url, handler) {
     this.routes.push({method, url, handler});
   }
-
+  resolve(context, request) {
+    let path = parse(request.url).pathname;
+    for (const {method, url, handler} of this.routes) {
+      let match = url.exec(path);
+      if (!match || request.method !== method) continue;
+      let urlParts = match.slice(1).map(decodeURIComponent);
+      return handler(context, ...urlParts, request);
+    }
+    return null;
+  }
 };
