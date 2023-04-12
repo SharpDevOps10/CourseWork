446c97b9ac45e56806478bc1beb95f0727cba2b8
SharpDevOps10 <td090833@gmail.com>
2023-04-12 15:57:01 +0300

    Improved code formatting using eslint

--- server/skills_server.js
+++ server/skills_server.js
@@ -1,23 +1,23 @@
-const {createServer} = require('node:http');
+const { createServer } = require('node:http');
 const Router = require('/router');
 const ecstatic = require('ecstatic');
 const router = new Router();
-const defaultHeaders = {"Content-Type" : "text/plain"};
+const defaultHeaders = { 'Content-Type': 'text/plain' };
 class SkillServer {
   constructor(talks) {
     this.talks = talks;
     this.version = 0;
     this.waiting = [];
-    let fileServer = ecstatic({root: "./public"});
+    const fileServer = ecstatic({ root: './public' });
     this.server = createServer((req, res) => {
       const resolved = router.resolve(this, req);
-      if(resolved) {
+      if (resolved) {
         resolved.catch((error) => {
           if (error.status !== null) return error;
-          return {body : String(error), status : 500};
-        }).then(({body,
-                     status = 200,
-                     headers = defaultHeaders}) => {
+          return { body: String(error), status: 500 };
+        }).then(({ body,
+          status = 200,
+          headers = defaultHeaders }) => {
           res.writeHead(status, headers);
           res.end(body);
         });
