c627e6fd64b009fea4e0d8d233c8af05dbb4c1e3
SharpDevOps10 <td090833@gmail.com>
2023-04-12 17:59:59 +0300

    Added `readStream` function to retrieve the content of a request body

--- server/skills_server.js
+++ server/skills_server.js
@@ -51,3 +51,12 @@ router.add("DELETE", talkPath, async (server, title) => {
   }
   return {status : 204};
 });
+const readStream = (stream) => new Promise((resolve, reject) => {
+  let data = "";
+  stream.on('error', reject);
+  stream.on('data', (chunk) => data+= chunk.toString());
+  stream.on('end', () => resolve(data));
+});
+
+
+
