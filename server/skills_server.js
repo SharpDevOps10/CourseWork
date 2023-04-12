36d1aa4a9fa077a6d14ab5c90a2716ac3f3a3b04
SharpDevOps10 <td090833@gmail.com>
2023-04-12 15:54:48 +0300

    Implemented constructor

--- server/skills_server.js
+++ server/skills_server.js
@@ -1,3 +1,31 @@
 const {createServer} = require('node:http');
 const Router = require('/router');
 const ecstatic = require('ecstatic');
+const router = new Router();
+const defaultHeaders = {"Content-Type" : "text/plain"};
+class SkillServer {
+  constructor(talks) {
+    this.talks = talks;
+    this.version = 0;
+    this.waiting = [];
+    let fileServer = ecstatic({root: "./public"});
+    this.server = createServer((req, res) => {
+      const resolved = router.resolve(this, req);
+      if(resolved) {
+        resolved.catch((error) => {
+          if (error.status !== null) return error;
+          return {body : String(error), status : 500};
+        }).then(({body,
+                     status = 200,
+                     headers = defaultHeaders}) => {
+          res.writeHead(status, headers);
+          res.end(body);
+        });
+      } else {
+        fileServer(req, res);
+      }
+    });
+
+  }
+
+}
