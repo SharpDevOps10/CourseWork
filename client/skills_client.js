877dd39e85a386fc74892f7890a2309b72020694
SharpDevOps10 <td090833@gmail.com>
2023-04-13 18:27:28 +0300

    Added `use strict`

--- client/skills_client.js
+++ client/skills_client.js
@@ -1,4 +1,4 @@
-
+'use strict';
 const fetchOK = (url, options) => fetch(url, options).then((response) => {
   if (response.status < 400) return response;
   else throw new Error(response.statusText);
