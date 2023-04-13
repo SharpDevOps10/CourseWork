d513828793d937c9cc12545ea065f19b4ae16b23
SharpDevOps10 <td090833@gmail.com>
2023-04-13 18:15:17 +0300

    Implemented `handleAction` function

--- client/skills_client.js
+++ client/skills_client.js
@@ -8,5 +8,38 @@ const talkURL = (title) => {
   return 'talks/' + encodeURIComponent(title);
 };
 
+const handleAction = (state, action) => {
+  if (action.type === 'setUser') {
+    localStorage.setItem('userName', action.user);
+    return Object.assign({}, state, {user: action.user});
+  } else if (action.type === 'setTalks') {
+    return Object.assign({}, state, {talks: action.talks});
+  } else if (action.type === 'newTask') {
+    fetchOK(talkURL(action.title), {
+      method : 'PUT',
+      headers : {'Content-Type' : 'application/json'},
+      body : JSON.stringify({
+        presenter : state.user,
+        summary : action.summary,
+      })
+
+    }).catch(reportError);
+
+  } else if (action.type === 'deleteTask') {
+    fetchOK(talkURL(action.talk), {method: 'DELETE'})
+      .catch(reportError);
+  } else if (action.type === 'newComment') {
+    fetchOK(talkURL(action.talk) + '/comments', {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify({
+        author: state.user,
+        message: action.message
+      })
+    }).catch(reportError);
+  }
+  return state;
+
+};
 
 
