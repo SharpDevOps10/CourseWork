7d127a16838015dbd076d5c8e4d0247d00f85a2a
SharpDevOps10 <td090833@gmail.com>
2023-04-18 11:32:54 +0300

    Implemented `PortfolioApp` class that ties the whole user interface together

--- client/skills_client.js
+++ client/skills_client.js
@@ -125,6 +125,29 @@ const renderTalkForm = (dispatch) => {
     elt('button', {type: 'submit'}, 'Submit'));
 };
 
+class PortfolioApp {
+  constructor(state, dispatch) {
+    this.dispatch = dispatch;
+    this.talkDOM = elt('div', {className: 'talks'});
+    this.dom = elt('div', null,
+        renderUserField(state.user, dispatch),
+        this.talkDOM,
+        renderTalkForm(dispatch));
+    this.syncState(state);
+  }
+  syncState(state) {
+    if (state.talks !== this.talks) {
+      this.talkDOM.textContent = '';
+      for (const talk of state.talks) {
+        this.talkDOM.appendChild(
+          renderTalk(talk, this.dispatch));
+      }
+      this.talks = state.talks;
+    }
+  }
+}
+
+
 
 
 
